3fef7a30eef37d7d24a39226a436f29e7a980168
adcarey <ad.carey1990@gmail.com>
2017-08-13 11:24:19 -0600

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,6 +5,7 @@ var difference = 0;
 
 module.exports = function(app){
 	app.get('/api/friends', function(req, res){
+		console.log(res);
 		res.json(friends);
 	});
 
@@ -25,20 +26,20 @@ module.exports = function(app){
 		//loop through the friends data array of objects to get each friends scores
 		for(var i = 0; i < [friends].length-1; i++){
 			console.log(friends[i].name);
-			totalDifference = 0;
+			difference = 0;
 
 			//loop through that friends score and the users score and calculate the 
 			// absolute difference between the two and push that to the total difference variable set above
 			for(var j = 0; j < 10; j++){
 				// We calculate the difference between the scores and sum them into the totalDifference
-				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
+				difference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
 				// If the sum of differences is less then the differences of the current "best match"
-				if (totalDifference <= greatMatch.friendDifference){
+				if (difference <= greatMatch.friendDifference){
 
 					// Reset the bestMatch to be the new friend. 
 					greatMatch.name = friends[i].name;
 					greatMatch.photo = friends[i].photo;
-					greatMatch.matchDifference = totalDifference;
+					greatMatch.matchDifference = difference;
 				}
 			}
 		}
